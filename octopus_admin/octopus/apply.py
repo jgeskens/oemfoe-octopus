91dac8f69efa87d8eb56b5e1947ceb80e0496c0b
Jef Geskens <jef.geskens@gmail.com>
2016-02-21 09:26:58 +0100

    Only do overwrites when needed

--- octopus_admin/octopus/apply.py
+++ octopus_admin/octopus/apply.py
@@ -31,18 +31,28 @@ def apply_port_forwards():
     defined_service_dirs = []
     for portfw in PortForward.objects.filter(enabled=True, service__enabled=True):
         service_dir = get_service_directory(portfw)
+        service_script = get_service_run_script(portfw)
+        defined_script_content = render_service_script(portfw).encode('utf-8')
+
         defined_service_dirs.append(service_dir)
 
         # Create/overwrite service script
-        need_restart = False
         if not service_dir in existing_service_dirs:
             os.makedirs(service_dir)
+            need_write = True
+            need_restart = False
         else:
-            need_restart = True
-        fh = open(get_service_run_script(portfw), 'wb')
-        fh.write(render_service_script(portfw).encode('utf-8'))
-        fh.close()
-        os.system('chmod +x {} > /dev/null'.format(get_service_run_script(portfw)))
+            fh = open(service_script, 'rb')
+            existing_script_content = fh.read()
+            fh.close()
+            need_restart = defined_script_content != existing_script_content
+            need_write = need_restart
+
+        if need_write:
+            fh = open(service_script, 'wb')
+            fh.write(defined_script_content)
+            fh.close()
+            os.system('chmod +x {} > /dev/null'.format(service_script))
         if need_restart:
             os.system('sv restart {} > /dev/null'.format(SERVICE_PREFIX + six.text_type(portfw.pk)))
 
